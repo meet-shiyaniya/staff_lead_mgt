b183e24e49a3e967afbe023a9632f3f9faab1f0d
darshandhandhukiya <darshandhandhukiya8@gmail.com>
2025-01-30 11:56:19 +0530

    HRM complete import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hr_app/Screen/Staff%20HR%20Screens/staff_Dash_Screen.dart';
+import 'package:hr_app/staff_HRM_module/Screen/Staff%20HR%20Screens/staff_Dash_Screen.dart';
+
 
 void main() {
 
