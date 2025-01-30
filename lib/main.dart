af23da4a3ecd8672fb98fc183dbe430fb194fb96
meet-shiyaniya <meetshiyaniya011@gmail.com>
2025-01-30 16:16:33 +0530

    main.dart darshan deleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hr_app/Inquiry_Management/Inquiry_Management_Screen.dart';
 import 'package:hr_app/staff_HRM_module/Screen/Staff%20HR%20Screens/staff_Dash_Screen.dart';
 
 
@@ -8,7 +7,7 @@ void main() {
   runApp(MaterialApp(
 
     debugShowCheckedModeBanner: false,
-    home: InquiryManagementScreen(),
+    home: staffDashScreen(),
 
 
   ));
