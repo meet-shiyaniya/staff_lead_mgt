a1ed3cb955c1fe2b79c597c4f09eee5d09ad4111
darshandhandhukiya <darshandhandhukiya8@gmail.com>
2025-02-01 12:40:05 +0530

    all change done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hr_app/social_module/login_screen/login_screen.dart';
 import 'package:hr_app/staff_HRM_module/Screen/Staff%20HR%20Screens/staff_Dash_Screen.dart';
 
+import 'Inquiry_Management/Inquiry_Management_Screen.dart';
 import 'dashboard.dart';
 
 
@@ -10,8 +11,10 @@ void main() {
   runApp(MaterialApp(
 
     debugShowCheckedModeBanner: false,
-    // home: staffDashScreen(),
+     // home: staffDashScreen(),
     home:Dashboard()
+    // home:InquiryManagementScreen(),
+    // home: LoginScreen(),
   )
   );
 
