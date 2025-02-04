e57f41e5cd01c2bad2cf5558d4d0e134d2b44086
meet-shiyaniya <meetshiyaniya011@gmail.com>
2025-02-04 12:39:09 +0530

    main.dart change

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hr_app/Inquiry_Management/Inquiry_Management_Screen.dart';
-import 'package:hr_app/social_module/chatting_module/example.dart';
-import 'package:hr_app/social_module/login_screen/login_screen.dart';
-
-
 import 'bottom_navigation.dart';
-import 'dashboard.dart';
 
 
 void main() {
