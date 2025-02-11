6935a95b603b6ef54fd4c68189e40a137fcb5d45
meet-shiyaniya <meetshiyaniya011@gmail.com>
2025-02-11 11:43:00 +0530

    notification ui updated.

--- lib/staff_HRM_module/Screen/Staff HR Screens/Notification/notification_Screen.dart
+++ lib/staff_HRM_module/Screen/Staff HR Screens/Notification/notification_Screen.dart
@@ -7,26 +7,27 @@ class NotificationScreen extends StatefulWidget {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
-  bool showUnread = false;
-
   final List<Map<String, dynamic>> notifications = [
     {
       'icon': Icons.attach_money,
       'title': 'USDT Deposit received',
       'description': 'You received \$370 USDT from 0xDc9AE3A3E3c8AcA2768e1690D...',
       'isUnread': true,
+      'isViewed': false,
     },
     {
       'icon': Icons.check_circle,
       'title': 'Donation Successful',
       'description': 'GTS foundation completes \$57,000 education fundraise.',
-      'isUnread': false,
+      'isUnread': true,
+      'isViewed': false,
     },
     {
       'icon': Icons.campaign,
       'title': 'Campaign Completed',
       'description': 'GTS foundation completes \$57,000 education fundraise.',
       'isUnread': true,
+      'isViewed': false,
     },
   ];
 
@@ -37,119 +38,75 @@ class _NotificationScreenState extends State<NotificationScreen> {
         backgroundColor: Colors.deepPurple.shade300,
         centerTitle: true,
         elevation: 0,
-        foregroundColor: Colors.transparent,
         leading: IconButton(
-
-          onPressed: (){
-            Navigator.pop(context);
-          },
-
-          icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white, size: 20,),
-
+          onPressed: () => Navigator.pop(context),
+          icon: Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white, size: 20),
         ),
-        title: Text('Notifications', style: TextStyle(color: Colors.white,fontFamily: "poppins_thin", fontSize: 18)),
+        title: Text('Notifications', style: TextStyle(color: Colors.white, fontSize: 18)),
       ),
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                _buildTab('All', !showUnread),
-                SizedBox(width: 16),
-                _buildTab('Unread', showUnread),
-              ],
-            ),
-            SizedBox(height: 16),
-            Expanded(
-              child: ListView(
-                children: notifications
-                    .where((notification) => !showUnread || notification['isUnread'])
-                    .map((notification) => _buildNotificationCard(notification))
-                    .toList(),
-              ),
-            ),
-          ],
+        child: Expanded(
+          child: ListView.builder(
+            itemCount: notifications.length,
+            itemBuilder: (context, index) {
+              return _buildNotificationCard(notifications[index], index);
+            },
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildTab(String label, bool isActive) {
+  Widget _buildNotificationCard(Map<String, dynamic> notification, int index) {
     return GestureDetector(
       onTap: () {
         setState(() {
-          showUnread = (label == 'Unread');
+          notifications[index]['isUnread'] = false;
+          notifications[index]['isViewed'] = true;
         });
       },
       child: Container(
-
+        height: 100,
+        margin: EdgeInsets.only(bottom: 12),
+        padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(color: Colors.black)
+          color: notification['isViewed'] ? Colors.deepPurple.shade50 : appColor.subFavColor,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [BoxShadow(color: Colors.grey.shade400, blurRadius: 3, offset: Offset(1, 3))],
         ),
-        padding: EdgeInsets.all(10),
-        child: Text(
-          label,
-          style: TextStyle(
-            fontSize: 14,
-            fontFamily: "poppins_thin",
-            // fontWeight: FontWeight.bold,
-            color: isActive ? Colors.deepPurple : Colors.grey,
-            // decoration: isActive ? TextDecoration.underline : TextDecoration.none,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildNotificationCard(Map<String, dynamic> notification) {
-    return Container(
-      height: 100,
-      margin: EdgeInsets.only(bottom: 12),
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-
-        color: appColor.subFavColor,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [BoxShadow(color: Colors.grey.shade400, blurRadius: 3, offset: Offset(1, 3))],
-
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: Colors.deepPurple.shade50,
-            child: Icon(notification['icon'], color: Colors.deepPurple),
-          ),
-          SizedBox(width: 12),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  notification['title'],
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontFamily: "poppins_thin",
-                    color: Colors.black87,
-                  ),
-                ),
-                SizedBox(height: 4),
-                Text(
-                  notification['description'],
-                  style: TextStyle(fontSize: 13.5, color: Colors.grey.shade600,fontFamily: "poppins_light"),
-                ),
-              ],
+        child: Row(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.deepPurple.shade50,
+              child: Icon(notification['icon'], color: Colors.deepPurple),
             ),
-          ),
-          if (notification['isUnread'])
-            Icon(Icons.circle, size: 10, color: Colors.deepPurple),
-        ],
+            SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    notification['title'],
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text(
+                    notification['description'],
+                    style: TextStyle(fontSize: 13.5, color: Colors.grey.shade600),
+                  ),
+                ],
+              ),
+            ),
+            if (notification['isUnread'])
+              Icon(Icons.circle, size: 10, color: Colors.deepPurple),
+          ],
+        ),
       ),
     );
   }
